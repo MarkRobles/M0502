865892d86c1c7105e263281c9485877edb7e7110
Marcos Robles <marcos92robles@gmail.com>
2020-03-06 16:33:58 -0600

    SubirArchivos con formularios

--- M0502/Controllers/CategoryController.cs
+++ M0502/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using M0502.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -30,7 +31,46 @@ namespace M0502.Controllers
                 
 
         }
-        
+
+
+
+        public ActionResult UploadPhoto()
+        {
+            var Category = new Models.Category();
+
+            return View(Category);
+        }
+
+        [HttpPost]
+        public ActionResult UploadPhoto2(Category newCategory, HttpPostedFileBase file)
+        {
+            ActionResult Result = View(newCategory);
+            /**Aqui deberia de estar la logica para validar y guardar en la base de datos*/
+            /*Aqui deberia estar la logica para recuperar el archivo*/
+            if(file!=null && file.ContentLength > 0)
+            {
+                Result = File(file.InputStream, file.ContentType);
+             
+            }
+            return Result;
 
         }
+
+
+        [HttpPost]
+        public ActionResult UploadPhoto(Category newCategory)
+        {
+            ActionResult Result = View(newCategory);
+            /**Aqui deberia de estar la logica para validar y guardar en la base de datos*/
+            /*Aqui deberia estar la logica para recuperar el archivo*/
+            if (Request.Files.Count > 0 != null && Request.Files[0].ContentLength > 0)
+            {
+                Result = File(Request.Files[0].InputStream, Request.Files[0].ContentType);
+
+            }
+            return Result;
+
+        }
+
+    }
     }
